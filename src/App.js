72ffb3e26c7e6325d05cace0ce6bfb92ac0ef2a0
biswajit-test <biswajit@test.com>
2022-03-31 12:59:18 +0530

    notification commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Notification from './firebaseNotifications/Notification';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Notification Application</h1>
+      <Notification />
+    </div>
+  );
+}
+
+export default App;
